4f7638537f62d812fc7f3fbf9a5b0efbdd8d459a
Andrew Rojas <drewr.dev@gmail.com>
2023-01-26 18:25:05 +0100

    incorporation of modules and styles of create benefits and statistics

---
+++ src/app/pages/create-benefits/create-benefits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-benefits',
+  templateUrl: './create-benefits.component.html',
+  styleUrls: ['./create-benefits.component.scss']
+})
+export class CreateBenefitsComponent {
+
+}
